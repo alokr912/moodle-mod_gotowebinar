df1adce52ed43344285756f97afdd18271ff20b9
Alok Kumar <alokr.mail@gmail.com>
2017-12-23 16:27:24 +0530

    Commit after upgrade file added

--- version.php
+++ version.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * GoToWebinar module version information file
  *
@@ -11,3 +12,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version = 2017070900;       // The current module version (Date: YYYYMMDDXX).
 $plugin->requires = 2014050800;    // Requires this Moodle version.
 $plugin->component = 'mod_gotowebinar';   // Full name of the plugin (used for diagnostics).
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->release = 'v2.7-r0';
