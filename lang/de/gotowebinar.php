44b6cea3be083fb686fa4b5d1f487b5b445e9afb
Vincent Schneider <vincent_tom.schneider@th-wildau.de>
2019-11-08 12:25:14 +0100

    added german language pack

---
+++ lang/de/gotowebinar.php
@@ -0,0 +1,38 @@
+<?php
+
+
+/**
+ * Strings for component 'GoToWebinar', language 'en',
+ *
+ * @package mod_gotowebinar
+ * @copyright 2017 Alok Kumar Rai <alokr.mail@gmail.com,alokkumarrai@outlook.in>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['pluginname'] = 'GoToWebinar';
+$string['modulename'] = 'GoToWebinar';
+$string['modulenameplural'] = 'GoToWebinar';
+$string['gotowebinarheading'] = 'GoToWebinar-Einstellungen';
+$string['gtw_consumer_key'] = 'GoToWebinar-Consumer-Schlüssel';
+$string['gtw_consumer_key_desc'] = 'Geben Sie den GoToWebinar-Consumer-Schlüssel ein, der beim Erstellen der App unter https://goto-developer.logmeininc.com/user/me/apps generiert wird. ';
+$string['gtw_consumer_secret'] = 'GoToWebinar Consumer Secret';
+$string['gtw_consumer_secret_desc'] = 'Geben Sie das GoToWebinar-Consumer-Secret ein, das beim Erstellen der App unter https://goto-developer.logmeininc.com/user/me/apps generiert wird.';
+$string['gtw_userid'] = 'GoToWebinar Userid';
+$string['gtw_userid_desc'] = 'Geben Sie Ihre GoToWebinar-Benutzer-ID ein';
+$string['gtw_password'] = 'Passwort für GoToWebinar';
+$string['gtw_password_desc'] = 'Geben Sie Ihr GoToWebinar-Passwort ein';
+$string['gtw_enable'] = 'GoToWebinar aktivieren';
+$string['gtw_enable_desc'] = 'Klicken Sie auf das Kontrollkästchen, um GoToWebinar zu aktivieren.';
+$string['testconnection'] = 'Konfiguration und Verbindung testen';
+$string['generalsetting'] = 'Allgemeine Einstellung';
+$string['startdatetime'] = 'Startdatum und -zeit';
+$string['enddatetime'] = 'Enddatum und -zeit';
+$string['meetingname'] = 'Name';
+$string['general'] = 'Allgemein';
+$string['meetingnamerequired'] = 'Besprechungsname ist erforderlich';
+$string['gotolmsintro'] = 'Beschreibung';
+$string['meetingtitle'] = 'Title';
+$string['meetingheader'] = 'GoToWebinar-Einstellung';
+$string['gotowebinarintro'] = 'Beschreibung';
+$string['meetingheader'] = 'GoToWebinar-Einstellung';
+$string['gotowebinarintrorequired'] = '';
+$string['pluginadministration'] = '';
